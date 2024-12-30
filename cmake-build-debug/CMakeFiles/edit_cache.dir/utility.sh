e9a5f5f33535215fa5a41b37b4dde40620b009da
Fiamma <stavtxxx1@gmail.com>
2024-12-30 17:58:44 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/User/CLionProjects/EX3CPP/cmake-build-debug
+/cygdrive/c/Users/User/AppData/Local/JetBrains/CLion2024.3/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
